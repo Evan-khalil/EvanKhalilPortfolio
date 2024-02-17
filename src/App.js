85fcad2fe24b92fc25820f1ecb397a607dfc68f6
Evan-khalil <83017187+Evan-khalil@users.noreply.github.com>
2024-02-17 17:43:09 +0100

    sad

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
 import About from './Components/About';
+import Timeline from './Components/Timeline';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <main>
         <About />
+        <Timeline/>
       </main>
     </div>
   );
