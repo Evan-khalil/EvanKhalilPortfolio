56d3c686eca45d0d5bb6acdf0de2e2d59b3b3172
Evan-khalil <83017187+Evan-khalil@users.noreply.github.com>
2024-03-02 02:29:40 +0100

    dsa

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect, useRef } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import {faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+
 const Container = styled.div`
   height: 600px;
   margin: 0;
@@ -13,6 +14,7 @@ const Container = styled.div`
   justify-items: center;
   padding-top: 10%;
   padding-bottom: 50%;
+  position: relative; /* Added position relative for arrow positioning */
 `;
 
 const Title = styled.h1`
@@ -26,7 +28,7 @@ const CarouselContainer = styled.main`
   grid-row: 2 / 3;
   grid-column: 1 / 8;
   width: 100%;
-  height: 100%;
+  height: 160%;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -35,31 +37,73 @@ const CarouselContainer = styled.main`
   perspective: 600px;
   position: relative;
   user-select: none; /* Disable text selection */
+  cursor: grab; /* Change cursor to grab */
+  &:active {
+    cursor: grabbing; /* Change cursor to grabbing when clicked */
+  }
+  &:hover .arrow {
+    opacity: 1; /* Show arrows on hover */
+  }
 `;
+
+const ArrowAnimation = keyframes`
+  0%, 100% {
+    opacity: 0.5;
+    transform: translateY(0);
+  }
+  50% {
+    opacity: 1;
+    transform: translateY(-10px);
+  }
+`;
+
+const Arrow = styled(FontAwesomeIcon)`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  font-size: 34px;
+  color: white;
+  opacity: 0.5;
+  cursor: pointer;
+  animation: ${ArrowAnimation} 1s infinite;
+  z-index: 10; /* Adjusted z-index to be higher than videos */
+`;
+
+const LeftArrow = styled(Arrow)`
+  left: 22px;
+`;
+
+const RightArrow = styled(Arrow)`
+  right: 22px;
+`;
+
 
 const Item = styled.div`
   position: absolute;
-  width: 300px; /* Adjusted width */
-  height: 400px; /* Adjusted height */
+  width: 300px;
+  height: 400px;
   background-color: coral;
-  transition: transform 0.4s ease; /* Adjusted transition timing for smoother animation */
+  transition: transform 0.4s ease;
   transform: ${props =>
-    `rotateY(calc(10deg * ${props.currentPosition - props.position})) translateX(calc(350px * ${props.currentPosition - props.position}))`}; /* Adjusted translation */
+    `rotateY(calc(-9deg * ${props.currentPosition - props.position})) translateX(calc(310px * ${props.currentPosition - props.position})) ${props.currentPosition === props.position ? 'scale(1.3)' : 'scale(1)'}`}; /* Adjusted space between videos and added scale transform */
   z-index: ${props => 5 - Math.abs(props.position - props.currentPosition)};
-  cursor: pointer;
+  cursor: grabbing;
 `;
+
+
+
 
 const VideoContainer = styled.video`
   width: 100%;
   height: 100%;
-  touch-action: pan-y; /* Allow vertical scrolling */
-  background-color: black; /* Set background color to black */
+  touch-action: pan-y;
+  background-color: black;
 `;
 
 const VideoLink = styled.a`
   background-color: white;
   position: absolute;
-  bottom: -41px; /* Adjust as needed */
+  bottom: -41px;
   left: 50%;
   transform: translateX(-50%);
   z-index: 1;
@@ -75,16 +119,15 @@ const VideoLink = styled.a`
   height: 40px;
   cursor: pointer;
   transition: all 0.3s ease;
-
   &:hover {
     transform: translateX(-50%) rotate(180deg);
     box-shadow: 0 0 20px rgba(255, 255, 255, 0.5);
   }
 `;
-const YoutubeIcon = styled(FontAwesomeIcon)`
 
+const YoutubeIcon = styled(FontAwesomeIcon)`
   font-size: 5em;
-  color: #FF0000;
+  color: #ff0000;
 `;
 
 const videos = [
@@ -144,6 +187,16 @@ const Carousel = () => {
     });
   };
 
+  const handleLeftArrowClick = () => {
+    setCurrentPosition(currentPosition - 1);
+    pauseAllVideosExcept(-1);
+  };
+
+  const handleRightArrowClick = () => {
+    setCurrentPosition(currentPosition + 1);
+    pauseAllVideosExcept(-1);
+  };
+
   const handleTouchStart = e => {
     setStartX(e.touches[0].clientX);
     setIsSwiping(true);
@@ -195,6 +248,7 @@ const Carousel = () => {
   };
 
   const handleItemClick = index => {
+    setCurrentPosition(index); // Set currentPosition to the index of the clicked video
     const player = playerRefs.current[index];
     if (activeVideoIndex === index) {
       if (player.paused) {
@@ -208,6 +262,7 @@ const Carousel = () => {
       setActiveVideoIndex(index);
     }
   };
+  
 
   const pauseAllVideosExcept = indexToKeepPlaying => {
     playerRefs.current.forEach((player, index) => {
@@ -221,7 +276,6 @@ const Carousel = () => {
   return (
     <Container id="Projects">
       <Title>Project highlights</Title>
-      
       <CarouselContainer
         onTouchStart={handleTouchStart}
         onTouchMove={handleTouchMove}
@@ -230,6 +284,8 @@ const Carousel = () => {
         onMouseMove={handleMouseMove}
         onMouseUp={handleMouseUp}
       >
+        <LeftArrow icon={faArrowLeft} className="arrow" onClick={handleLeftArrowClick} />
+        <RightArrow icon={faArrowRight} className="arrow" onClick={handleRightArrowClick} />
         {videos.map((video, index) => (
           <Item
             key={index}
