6da5f23b2a0d926d98bd76cd95feb3b3709030d1
Evan-khalil <83017187+Evan-khalil@users.noreply.github.com>
2024-03-10 12:10:37 +0100

    f

--- src/Components/edu.js
+++ src/Components/edu.js
@@ -64,54 +64,54 @@ const CenteredFlipCards = () => {
       <div className="center-container">
         <div className="flip-container">
           <FlipCard
-            frontColor="#006AA7"
-            backColor="#006AA7"
+            frontColor="#F5DEB3"
+            backColor="#BC8F8F"
             backTitle="Bachelor's programme in Information Systems"
             backContent="Knowledge about people, organizations, and how to develop technology to support them alongside learning the basics of systems analysis and software development. The program offers courses in object-oriented analysis and design, agile methods, interaction design, database management, and programming in C# and Java."
             additionalText="Örebro University"
           />
           <FlipCard
-            frontColor="#FECC02"
-            backColor="#FECC02"
+            frontColor="#BC8F8F"
+            backColor="#D2B48C"
             backTitle="Programming for Internet"
             backContent="Gain expertise in using C# to program with supportive technologies, establish low-level connections to various servers, establish high-level connections to specialized servers like email, web, and database servers, and ensure security while communicating over the internet."
-            additionalText="Stokholm University"
+            additionalText="Stockholm University"
           />
           <FlipCard
-            frontColor="#006AA7"
-            backColor="#006AA7"
+            frontColor="#DEB887"
+            backColor="#F5DEB3"
             backTitle="Web Development II"
             backContent="Learn to build sophisticated web-based systems using JavaScript and PHP for both client- and server-side programming in this course."
-            additionalText="Stokholm University"
+            additionalText="Stockholm University"
           />
         </div>
 
         <div className="flip-container">
           <div className="flip flip-vertical">
-            <div className="front" style={{ backgroundColor: "#006AA7" }}>
+            <div className="front" style={{ backgroundColor: "#BC8F8F" }}>
               <h1 className="text-shadow">Programming in C#, III</h1>
             </div>
-            <div className="back" style={{ backgroundColor: "#006AA7" }}>
+            <div className="back" style={{ backgroundColor: "#F5DEB3" }}>
               <h2>Programming in C#, III</h2>
               <p>This course is designed for advanced programmers looking to sharpen their skills and develop applications using the advanced features of the C# language and .NET technologies.</p>
               <span className="additional-text">Malmö university</span>
             </div>
           </div>
           <div className="flip flip-vertical">
-            <div className="front" style={{ backgroundColor: "#FECC02" }}>
+            <div className="front" style={{ backgroundColor: "#D2B48C" }}>
               <h1 className="text-shadow">Computer Networks I</h1>
             </div>
-            <div className="back" style={{ backgroundColor: "#FECC02" }}>
+            <div className="back" style={{ backgroundColor: "#BC8F8F" }}>
               <h2>Computer Networks I</h2>
               <p>This course provides a foundation in the theory and practice of configuring and securing computer networks, including instruction on creating basic network topologies.</p>
               <span className="additional-text">Mälardalen university</span>
             </div>
           </div>
           <div className="flip flip-vertical">
-            <div className="front" style={{ backgroundColor: "#006AA7" }}>
+            <div className="front" style={{ backgroundColor: "#F5DEB3" }}>
               <h1 className="text-shadow">Game Development - 3D Modelling and Character Animation</h1>
             </div>
-            <div className="back" style={{ backgroundColor: "#006AA7" }}>
+            <div className="back" style={{ backgroundColor: "#DEB887" }}>
               <h2>Game Development - 3D Modelling and Character Animation</h2>
               <p>A course provides a foundational understanding and hands-on experience in creating game graphics using 2D and 3D tools, including techniques for rigging and animating characters and their movements for digital games.</p>
               <span className="additional-text">Blekinge college</span>
